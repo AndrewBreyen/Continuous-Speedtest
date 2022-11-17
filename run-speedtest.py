e589420fb3d8e51ac28172745dd7e6a12f45af04
Andrew Breyen <andrew.breyen@gmail.com>
2022-11-16 19:56:37 -0600

    down/up

--- run-speedtest.py
+++ run-speedtest.py
@@ -5,5 +5,18 @@ import speedtest
 
 st = speedtest.Speedtest()
 
+expected_down = 400
+expected_up = 10
+
+print("Running Test......")
 down = st.download()
-print(str(round(down, 2)) + " bytes/sec")
+up = st.upload()
+
+print ("Down")
+print(str(round(down, 2)) + " byte/sec")
+print(str(round(down/1000000, 2)) + " Mb/sec")
+
+print ('--------')
+print ("Up")
+print(str(round(up, 2)) + " byte/sec")
+print(str(round(up/1000000, 2)) + " Mb/sec")
