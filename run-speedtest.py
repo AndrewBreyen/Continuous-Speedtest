d826f2b05f73fdb22039469127489332c403bd7f
Andrew Breyen <andrew.breyen@gmail.com>
2022-11-16 21:41:00 -0600

    loop!

--- run-speedtest.py
+++ run-speedtest.py
@@ -4,6 +4,8 @@ import speedtest
 import csv
 from os.path import exists
 from datetime import datetime
+import time
+from os import system
 
 results_file = "results.csv"
 
@@ -25,41 +27,50 @@ st = speedtest.Speedtest()
 expected_down_mbps = 400
 expected_up_mbps = 10
 
-########
-# DOWN #
-########
-print("Running Test DOWN......")
-down = st.download(threads=4)
-down_bps = round(down, 2)
-down_mbps = down_bps/1000000
-down_percent_of_expected = round((down_mbps/expected_down_mbps)*100,2)
+loops = 0
+while True:
+	system('clear')
+	print ('loops: '+str(loops))
+	########
+	# DOWN #
+	########
+	print("Running Test DOWN......")
+	down = st.download(threads=4)
+	down_bps = round(down, 2)
+	down_mbps = down_bps/1000000
+	down_percent_of_expected = round((down_mbps/expected_down_mbps)*100,2)
 
-print ("Down:")
-print(str(down_bps) + " bytes/sec")
-print(str(down_mbps) + " Mb/sec")
-print(str(down_percent_of_expected)+"% of expected")
-
-
-######
-# UP #
-######
-print("Running Test UP......")
-up = st.upload(threads=4)
-up_bps = round(up, 2)
-up_mbps = up_bps/1000000
-up_percent_of_expected = round((up_mbps/expected_up_mbps)*100,2)
-
-print ("Up:")
-print(str(up_bps) + " bytes/sec")
-print(str(up_mbps) + " Mb/sec")
-print(str(up_percent_of_expected)+"% of expected")
+	print ("Down:")
+	print(str(down_bps) + " bytes/sec")
+	print(str(down_mbps) + " Mb/sec")
+	print(str(down_percent_of_expected)+"% of expected")
 
 
-#########################
-# WRITE RESULTS TO FILE #
-#########################
-with open(results_file,"a",newline="") as csvfile:
-    # creating writer object
-    csv_writer=csv.writer(csvfile)
-    # appending data
-    csv_writer.writerow([datetime.now(), down_mbps, down_percent_of_expected, up_mbps, up_percent_of_expected]) 
+	######
+	# UP #
+	######
+	print("Running Test UP......")
+	up = st.upload(threads=4)
+	up_bps = round(up, 2)
+	up_mbps = up_bps/1000000
+	up_percent_of_expected = round((up_mbps/expected_up_mbps)*100,2)
+
+	print ("Up:")
+	print(str(up_bps) + " bytes/sec")
+	print(str(up_mbps) + " Mb/sec")
+	print(str(up_percent_of_expected)+"% of expected")
+
+
+	#########################
+	# WRITE RESULTS TO FILE #
+	#########################
+	with open(results_file,"a",newline="") as csvfile:
+	    # creating writer object
+		csv_writer=csv.writer(csvfile)
+		# appending data
+		csv_writer.writerow([datetime.now(), down_mbps, down_percent_of_expected, up_mbps, up_percent_of_expected]) 
+		csvfile.close
+	
+	print ('Sleeping for 5 min...')
+	time.sleep(300)
+	loops+=1
